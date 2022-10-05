dc2e0ff760f9b3c48f8910c9e8d8f15678a90ab6
Sebastián Valencia G <sevalenciagr@gmail.com>
2022-10-05 17:23:48 -0500

    Cambio de funciones al boton Read More

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ function changeColor(newColor) {
 let firstHeading = document.querySelector('h1');
 
 console.log(firstHeading);
+
+// const btnReadMore = document.getElementById('btnReadMore');
+
+
+// btnReadMore.append(img2);
+
+
+const RMpSectionB = document.getElementById('RMpSectionB');
+const btnReadMore = document.querySelector('#btnReadMore');
+const img2 = document.createElement('img');
+
+img2.setAttribute('src', 'https://www.dsbnoticias.com/wp-content/uploads/2022/09/NASA-ASTEROIDE-1024x683.png');
+
+function bntReadmoreOnClick(){
+  RMpSectionB.append(img2);
+}
