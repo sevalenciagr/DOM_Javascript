5214a2eb1e14f04b33f9c974079d44fe8825bd39
CamiloGiraldoM <camiloandresgiraldo000@gmail.com>
2022-10-06 13:05:54 -0500

    Aplicando estilos a las imagenes y reorganizando el DOM

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ const imgText = document.getElementById('textOnClick');
 img2.setAttribute('src', 'https://www.dsbnoticias.com/wp-content/uploads/2022/09/NASA-ASTEROIDE-1024x683.png');
 img2.setAttribute('class', 'img');
 
-// Agregando imagen y texto despues de Onclick en el boton read more
+// Agregando imagen y texto despues de Onclick en el boton readme
 function bntReadmoreOnClick(){
   RMpSectionB.append(img2);
   imgText.innerText = "Armageddon es una película de ciencia ficción y cine catástrofe de 1998, dirigida por Michael Bay, producida por Jerry Bruckheimer y protagonizada por Bruce Willis, Billy Bob Thornton, Ben Affleck, Liv Tyler, Owen Wilson, Will Patton, Michael Clarke Duncan, Peter Stormare y Steve Buscemi. La película trata sobre un grupo de perforadores de plataformas petrolíferas que son enviados por la NASA a un enorme asteroide que amenaza el planeta Tierra con la idea de taladrar su superficie y poder destruirlo con una bomba nuclear."
@@ -36,8 +36,10 @@ const text = document.createTextNode('Una estrella es un esferoide luminoso que 
 const text2 = document.createTextNode(' Durante al menos una parte de su vida, una estrella brilla debido a la fusión termonuclear del hidrógeno en helio en su núcleo, que libera energía, la cual atraviesa el interior de la estrella y, después, se irradia hacia el espacio exterior. Casi todos los elementos naturales más pesados que el helio se crean por nucleosíntesis estelar durante la vida de una estrella y, en algunas de ellas, por nucleosíntesis de supernova cuando explotan.');
 
  function bntclickestrellaOnClick(){
+    RMpSectionC.append(img3);
     RMpSectionC.append(text);  
     RMpSectionC.append(text2);
+    
 
     //  console.log(pid);
     //  pid.append(img4);
